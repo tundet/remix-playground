403325968917b1d116138b1278b6153161824fe5
tundet <ryunde.taba@gmail.com>
2024-08-04 14:23:27 +0300

    use vite env var

--- apollo/apolloClient.tsx
+++ apollo/apolloClient.tsx
@@ -1,15 +1,18 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client/index.js";
 import { setContext } from "@apollo/client/link/context";
 
+const accessToken = import.meta.env.VITE_ACCESS_TOKEN;
+const contentUrl = import.meta.env.VITE_CONTENT_URL;
+
 const httpLink = createHttpLink({
-  uri: process.env.CONTENT_URL,
+  uri: contentUrl,
 });
 
 const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+      Authorization: `Bearer ${accessToken}`,
     },
   };
 });
